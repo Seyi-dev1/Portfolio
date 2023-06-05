843f02ad4f2ccb132234293cafac6a92841a4d45
seyi-dev1 <samoluwaseyi25gmail.com>
2023-06-05 12:25:35 +0100

    testing UI changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import styles from "./work.module.scss";
 import Work from "../components/work/Work";
 
+export const metadata = {
+  title: "Seyi - software engineer",
+  description: "portfolio website for software engineer.",
+};
+
 const WorkPage = () => {
   return (
     <div className={styles.work}>
