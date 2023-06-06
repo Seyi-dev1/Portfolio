6d86a55776059ddfcfc73d4cb916f8725fbd8713
seyi-dev1 <samoluwaseyi25gmail.com>
2023-06-06 01:18:25 +0100

    more UI changes

--- app/components/responsive home/ResponsiveBio.jsx
+++ app/components/responsive home/ResponsiveBio.jsx
@@ -1,4 +1,3 @@
-"use client";
 import styles from "./responsiveBio.module.scss";
 import avatar2 from "../../../assets/fotor-ai-2023060322410.jpg";
 import { BsGithub } from "react-icons/bs";
@@ -6,34 +5,9 @@ import { BsArrowUpRight } from "react-icons/bs";
 import { BsTwitter, BsFillPlayFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
 const ResponsiveBio = () => {
-  const [width, setWidth] = useState(1300);
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleWindowResize);
-
-    function preventScroll(e) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      return false;
-    }
-    width > 1200 &&
-      document.querySelector("#bio").addEventListener("wheel", preventScroll, {
-        passive: false,
-      });
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  });
-
   return (
     <div className={styles.home}>
       <div className={styles.container}>
@@ -43,14 +17,14 @@ const ResponsiveBio = () => {
               <Image src={avatar2} alt="avatar" />
             </div>
             <h1 className={styles.greeting}>Hello, I'm Oluwaseyi Samuel.</h1>
-            <p className={styles.intro}>
+            {/* <p className={styles.intro}>
               I'm a <span>software engineer</span> who loves making cool and fun{" "}
               <span>web apps</span>. I know how to use HTML, CSS, JavaScript,
               and a bunch of <span>frontend frameworks</span> like{" "}
               <span>React</span>, <span>Angular</span>, <span>Vue</span>, and{" "}
               <span>Svelte</span>. I'm always curious about new stuff and how to
               make the web better for <span>everyone</span>.
-            </p>
+            </p> */}
             <div className={styles.explorebtn}>
               <Link href={"/work"}>
                 <button>
